138f33ee5f4e331745589c921bcbc67c3a5c186e
Arsène von Wyss <arsene.vonwyss@sirius.ch>
2019-01-13 14:19:39 +0100

    Optimized range set condition emit for consecutive ranges with just one missing item in between

--- src/Sirius.Common/StateMachine/RangeConditionEmitter.cs
+++ src/Sirius.Common/StateMachine/RangeConditionEmitter.cs
@@ -15,6 +15,16 @@ namespace Sirius.StateMachine {
 
 		private static readonly MethodInfo meth_IComparable_Compare = Reflect<IComparable<TInput>>.GetMethod(v => v.CompareTo(default));
 
+		internal static BinaryExpression EmitSingleValueCompare(ParameterExpression varInput, TInput value) {
+			return typeof(TInput).IsPrimitive
+					? Expression.Equal(
+							varInput,
+							Expression.Constant(value))
+					: Expression.Equal(
+							Expression.Call(varInput, meth_IComparable_Compare, Expression.Constant(value)),
+							Expression.Constant(0));
+		}
+
 		/// <summary>Emits the condition to compare a single input against a range.</summary>
 		/// <param name="comparand">The comparand.</param>
 		/// <param name="varInput">The variable input.</param>
@@ -22,13 +32,7 @@ namespace Sirius.StateMachine {
 		public Expression Emit(Range<TInput> comparand, ParameterExpression varInput) {
 			switch (comparand.Expand().Take(3).Count()) {
 			case 1:
-				return typeof(TInput).IsPrimitive
-						? Expression.Equal(
-								varInput,
-								Expression.Constant(comparand.From))
-						: Expression.Equal(
-								Expression.Call(varInput, meth_IComparable_Compare, Expression.Constant(comparand.From)),
-								Expression.Constant(0));
+				return EmitSingleValueCompare(varInput, comparand.From);
 			case 2:
 				return typeof(TInput).IsPrimitive
 						? Expression.OrElse(
--- src/Sirius.Common/StateMachine/RangesConditionEmitter.cs
+++ src/Sirius.Common/StateMachine/RangesConditionEmitter.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Linq.Expressions;
 
 using Sirius.Collections;
@@ -9,23 +11,52 @@ namespace Sirius.StateMachine {
 	/// <typeparam name="TComparand">Type of the comparand.</typeparam>
 	/// <typeparam name="TInput">Type of the input.</typeparam>
 	public class RangesConditionEmitter<TComparand, TInput>: IConditionEmitter<TComparand, TInput>
-			where TComparand: IEnumerable<Range<TInput>>
+			where TComparand: IRangeSet<TInput>
 			where TInput: IComparable<TInput> {
 		/// <summary>The default ranges condition emitter.</summary>
 		public static readonly RangesConditionEmitter<TComparand, TInput> Default = new RangesConditionEmitter<TComparand, TInput>();
+
+		private static IEnumerator<Expression> EmitRangesWithExclusions(TComparand comparand, ParameterExpression varInput) {
+			var exclusions = new List<TInput>();
+			var decrement = Incrementor<TInput>.Decrement;
+			var increment = Incrementor<TInput>.Increment;
+			using (var enumerator = comparand.GetEnumerator()) {
+				if (!enumerator.MoveNext()) {
+					yield break;
+				}
+				bool hasNext;
+				do {
+					var currentRange = enumerator.Current;
+					exclusions.Clear();
+					for (hasNext = enumerator.MoveNext(); hasNext; hasNext = enumerator.MoveNext()) {
+						var candidate = increment(currentRange.To);
+						if (!candidate.Equals(decrement(enumerator.Current.From))) {
+							break;
+						}
+						exclusions.Add(increment(currentRange.To));
+						currentRange = Range<TInput>.Create(currentRange.From, enumerator.Current.To);
+					}
+					yield return exclusions.Aggregate(
+							RangeConditionEmitter<TInput>.Default.Emit(currentRange, varInput),
+							(expr, excl) => Expression.AndAlso(expr, RangeConditionEmitter<TInput>.EmitSingleValueCompare(varInput, excl)));
+				} while (hasNext);
+			}
+		}
 
 		/// <summary>Emits the condition to compare a single input against a set of ranges.</summary>
 		/// <param name="comparand">The comparand.</param>
 		/// <param name="varInput">The variable input.</param>
 		/// <returns>An Expression.</returns>
 		public Expression Emit(TComparand comparand, ParameterExpression varInput) {
-			using (var enumerator = comparand.GetEnumerator()) {
+			using (var enumerator = EmitRangesWithExclusions(comparand, varInput)) {
 				if (!enumerator.MoveNext()) {
 					return Expression.Constant(false);
 				}
-				var result = RangeConditionEmitter<TInput>.Default.Emit(enumerator.Current, varInput);
+				var result = enumerator.Current;
+				Debug.Assert(result != null);
 				while (enumerator.MoveNext()) {
-					var condition = RangeConditionEmitter<TInput>.Default.Emit(enumerator.Current, varInput);
+					var condition = enumerator.Current;
+					Debug.Assert(condition != null);
 					result = (condition.NodeType == ExpressionType.OrElse) && condition is BinaryExpression orElseCondition
 							? Expression.OrElse(
 									Expression.OrElse(result,
